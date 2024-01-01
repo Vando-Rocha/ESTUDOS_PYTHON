913fe8eb32f24fc1bc9bbf87e4ae95365e9a0674
Vando-Rocha <vandorcdev@gmail.com>
2024-01-01 20:38:31 -0300

    Criado mensagens da tabela de massa corporal

--- calculadora_imc.py
+++ calculadora_imc.py
@@ -4,10 +4,32 @@ Autor: Vando Rocha
 Descrição: Trata-se de um programa que recebe do usuário o peso e altura e cálcula o índice de massa corporal
 """
 #Receber valores
-peso = float(input("Digite seu peso: "))
-altura = float(input("Digite sua altura: "))
+peso = float(input("Digite seu peso: ").replace(",", "."))
+altura = float(input("Digite sua altura: ").replace(",", "."))
+
 
 
 imc = peso/(altura**2)
 
-print(imc)
+print("IMC = ", round(imc,2))
+
+if imc <= 16.99:
+    print("Você está muito abaixo do peso.")
+    
+elif imc > 16.99 and imc < 18.49:
+    print("Você está abaixo do peso.")
+    
+elif imc > 18.49 and imc <= 24.99:
+    print("Peso normal.")
+    
+elif imc > 24.99 and imc <= 29.99:
+    print("Você está acima do peso.")
+    
+elif imc > 29.99 and imc <= 34.99:
+    print("Obesidade grau I.")
+
+elif imc > 34.99 and imc <= 39.99:
+    print("Obesidade grau II.")
+
+else:
+    print("Obesidade grau III.")
